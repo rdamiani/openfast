19dc98bd992e05a2eafa749ab519f607b418019e
Abhineet Gupta <abhineet.gupta@nrel.gov>
2026-06-05 15:15:49 -0600

    Remove debugging lines from NWTC_Lapack

--- modules/nwtc-library/src/NetLib/lapack/NWTC_LAPACK.f90
+++ modules/nwtc-library/src/NetLib/lapack/NWTC_LAPACK.f90
@@ -389,9 +389,6 @@ MODULE NWTC_LAPACK
       INTEGER(IntKi)                 :: ErrStat2          !< Error level
       CHARACTER(ErrMsgLen)           :: ErrMsg2           !< Message describing error
       CHARACTER(*), PARAMETER        :: RoutineName = 'LAPACK_SGELS'
-        #ifdef DEBUG
-         call ieee_set_halting_mode(ieee_divide_by_zero, .false.)
-        #endif
       
       ErrStat = ErrID_None
       ErrMsg  = ""
